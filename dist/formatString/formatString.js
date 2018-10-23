dca30fb9abec30e32620b852416b7ee3ad80b80b
Martin Kock <mk@omnicar.dk>
2018-10-23 10:53:54 +0200

    PR feedback

--- dist/formatString/formatString.js
+++ dist/formatString/formatString.js
@@ -1,42 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var init_1 = require("../init/init");
+// Usage guide:
+// phoneMap contains conversion patterns between given (expected) format and the format that is being converted into.
+// newFormat should follow the phone number layout as it will look WITH spaces added. The algorithm will remove spaces
+// by itself to produce the non-spaced version. When adding new formats, remember that 1) newFormat should probably
+// start with $2 as the area code (value $1) is added by the algorithm, and 2) you don't need to prefix newFormat
+// with an empty space in order to separate the main part of the number from the area code; again, the algorithm will
+// do this for you.
 var phoneMap = new Map();
-phoneMap.set('da-DK', formatDKPhone);
-phoneMap.set('sv-SE', formatSEPhone);
-function formatDKPhone(phone, opts) {
-    var expectedFormat = /(\+\d{2})?(\d{2})(\d{2})(\d{2})(\d{2})/g;
-    var newFormat = '$2$3$4$5';
-    if (opts.addSpaces) {
-        newFormat = ' $2 $3 $4 $5';
-    }
-    if (opts.showAreaCode) {
-        newFormat = '$1' + newFormat;
-    }
-    return phone.replace(expectedFormat, newFormat.trim());
-}
-function formatSEPhone(phone, opts) {
-    var expectedFormat = /(\+\d{2})?(\d{2})(\d{3})(\d{2})(\d{2})/g;
-    var newFormat = '$2$3$4$5';
-    if (opts.addSpaces) {
-        newFormat = ' $2-$3 $4 $5';
-    }
-    if (opts.showAreaCode) {
-        newFormat = '$1' + newFormat;
-    }
-    return phone.replace(expectedFormat, newFormat.trim());
-}
+phoneMap.set('da-DK', {
+    givenFormat: /(\+\d{2})?(\d{2})(\d{2})(\d{2})(\d{2})/g,
+    newFormat: '$2 $3 $4 $5',
+});
+phoneMap.set('sv-SE', {
+    givenFormat: /(\+\d{2})?(\d{2})(\d{3})(\d{2})(\d{2})/g,
+    newFormat: '$2-$3 $4 $5',
+});
+// formatPhone formats the given phone number according to locale. Supports spacing between number groups
+// and an option for whether area code should be displayed or not. The pattern '00<area-code>' will be changed
+// into '+<area-code>' as part of the normalization.
 function formatPhone(phone, _a) {
     var _b = _a === void 0 ? {} : _a, _c = _b.showAreaCode, showAreaCode = _c === void 0 ? true : _c, _d = _b.addSpaces, addSpaces = _d === void 0 ? true : _d;
     if (!phone) {
         return phone;
     }
-    var formatter = phoneMap.get(init_1.defaults.isoName);
-    if (!formatter) {
-        throw Error("You requested locale \"" + init_1.defaults.isoName + "\", but there are no phone formatters for that locale");
+    var formats = phoneMap.get(init_1.defaults.isoName);
+    if (!formats) {
+        throw Error("You requested locale \"" + init_1.defaults.isoName + "\", but there are no phone formats for that locale");
+    }
+    // Add space between area code and main number for spaced numbers.
+    var newFormat = addSpaces
+        ? ' ' + formats.newFormat
+        : formats.newFormat.replace(/[ \-+\.,]/g, '');
+    if (showAreaCode) {
+        newFormat = '$1' + newFormat;
     }
     // Preprocessing: remove spaces and replace '00' with '+' in area code.
     var normalizedPhone = phone.replace(/ /g, '').replace(/^([ ]+)?00/g, '+');
-    return formatter(normalizedPhone, { showAreaCode: showAreaCode, addSpaces: addSpaces }).trim();
+    return normalizedPhone.replace(formats.givenFormat, newFormat.trim()).trim();
 }
 exports.formatPhone = formatPhone;
--- src/formatString/formatString.ts
+++ src/formatString/formatString.ts
@@ -2,37 +2,29 @@ import { IsoLocale } from '@omnicar/sam-types'
 import { defaults } from '../init/init'
 import { IFormatStringOptions } from './IFormatStringOptions'
 
+// Usage guide:
+// phoneMap contains conversion patterns between given (expected) format and the format that is being converted into.
+// newFormat should follow the phone number layout as it will look WITH spaces added. The algorithm will remove spaces
+// by itself to produce the non-spaced version. When adding new formats, remember that 1) newFormat should probably
+// start with $2 as the area code (value $1) is added by the algorithm, and 2) you don't need to prefix newFormat
+// with an empty space in order to separate the main part of the number from the area code; again, the algorithm will
+// do this for you.
 const phoneMap: Map<
   IsoLocale,
-  (phone: string, opts: IFormatStringOptions) => string
+  { givenFormat: RegExp; newFormat: string }
 > = new Map()
-phoneMap.set('da-DK', formatDKPhone)
-phoneMap.set('sv-SE', formatSEPhone)
+phoneMap.set('da-DK', {
+  givenFormat: /(\+\d{2})?(\d{2})(\d{2})(\d{2})(\d{2})/g,
+  newFormat: '$2 $3 $4 $5',
+})
+phoneMap.set('sv-SE', {
+  givenFormat: /(\+\d{2})?(\d{2})(\d{3})(\d{2})(\d{2})/g,
+  newFormat: '$2-$3 $4 $5',
+})
 
-function formatDKPhone(phone: string, opts: IFormatStringOptions): string {
-  const expectedFormat = /(\+\d{2})?(\d{2})(\d{2})(\d{2})(\d{2})/g
-  let newFormat: string = '$2$3$4$5'
-  if (opts.addSpaces) {
-    newFormat = ' $2 $3 $4 $5'
-  }
-  if (opts.showAreaCode) {
-    newFormat = '$1' + newFormat
-  }
-  return phone.replace(expectedFormat, newFormat.trim())
-}
-
-function formatSEPhone(phone: string, opts: IFormatStringOptions): string {
-  const expectedFormat = /(\+\d{2})?(\d{2})(\d{3})(\d{2})(\d{2})/g
-  let newFormat: string = '$2$3$4$5'
-  if (opts.addSpaces) {
-    newFormat = ' $2-$3 $4 $5'
-  }
-  if (opts.showAreaCode) {
-    newFormat = '$1' + newFormat
-  }
-  return phone.replace(expectedFormat, newFormat.trim())
-}
-
+// formatPhone formats the given phone number according to locale. Supports spacing between number groups
+// and an option for whether area code should be displayed or not. The pattern '00<area-code>' will be changed
+// into '+<area-code>' as part of the normalization.
 export function formatPhone(
   phone: string,
   { showAreaCode = true, addSpaces = true } = {} as IFormatStringOptions,
@@ -40,15 +32,22 @@ export function formatPhone(
   if (!phone) {
     return phone
   }
-  const formatter = phoneMap.get(defaults.isoName)
-  if (!formatter) {
+  const formats = phoneMap.get(defaults.isoName)
+  if (!formats) {
     throw Error(
       `You requested locale "${
         defaults.isoName
-      }", but there are no phone formatters for that locale`,
+      }", but there are no phone formats for that locale`,
     )
+  }
+  // Add space between area code and main number for spaced numbers.
+  let newFormat = addSpaces
+    ? ' ' + formats.newFormat
+    : formats.newFormat.replace(/[ \-+\.,]/g, '')
+  if (showAreaCode) {
+    newFormat = '$1' + newFormat
   }
   // Preprocessing: remove spaces and replace '00' with '+' in area code.
   const normalizedPhone = phone.replace(/ /g, '').replace(/^([ ]+)?00/g, '+')
-  return formatter(normalizedPhone, { showAreaCode, addSpaces }).trim()
+  return normalizedPhone.replace(formats.givenFormat, newFormat.trim()).trim()
 }
